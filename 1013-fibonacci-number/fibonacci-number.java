4afa1e1bc6e3303edd6feb1c16f553402503127a
bouchraboud <151152911+bouchraboud@users.noreply.github.com>
2025-02-28 21:49:54 +0000

    Time: 0 ms (100.00%) | Memory: 40.9 MB (10.61%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,16 +1,37 @@
 class Solution {
     public int fib(int n) {
-        int[] memo=new int[n+1];
-        return fun(n,memo);
-    }
-    public int fun(int n,int[] memo){
-        int result;
-        if(memo[n]!=0) return memo[n];
-        else if(n==0) result=0;
-        else if(n==1) result=1;
-        else result=fun(n-1,memo)+fun(n-2,memo);
-        memo[n]=result;
-        return result;
+        // int[] memo=new int[n+1];
+        // return fun(n,memo);
+         return BottomUp(n);
 
     }
+    public int BottomUp(int n){
+         int[] buttomUp=new int[n+1];
+         if(n==0) return 0;
+         if(n==1) return 1;
+         buttomUp[0]=0;
+         buttomUp[1]=1;
+         for(int i=2;i<=n;i++){
+            buttomUp[i]=buttomUp[i-1]+buttomUp[i-2];
+         }
+         return buttomUp[n];
+    }
+
+
+
+
+
+
+
+
+//     public int memoized(int n,int[] memo){
+//         int result;
+//         if(memo[n]!=0) return memo[n];
+//         else if(n==0) result=0;
+//         else if(n==1) result=1;
+//         else result=memoized(n-1,memo)+memoized(n-2,memo);
+//         memo[n]=result;
+//         return result;
+
+//     }
 }
