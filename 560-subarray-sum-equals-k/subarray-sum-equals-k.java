94f7863c25469e1c4448004386664d75a0cb6d9a
bouchraboud <151152911+bouchraboud@users.noreply.github.com>
2025-02-28 15:04:52 +0000

    Time: 24 ms (74.43%) | Memory: 47.5 MB (5.21%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,26 +1,27 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int result=0;
-        for(int i=1;i<nums.length;i++){
-            nums[i]=nums[i]+nums[i-1];
-        }
-       
+        int result = 0;
+        int sum=nums[0]; 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 1);
+        if (nums[0] == k)
+            result++;
+        for (int i = 1; i < nums.length; i++) {
+             sum = nums[i] + sum; // Calculate sum of consecutive elements
+            
+            if (sum == k)
+                result++;
+            if (map.containsKey(sum - k)) {
+                result += map.get(sum - k);
 
-        for(int i=0;i<nums.length;i++){
-            for(int j=i;j<nums.length;j++){
-                if(i==0){
-                    
-                    if(nums[j]==k) {
-                       
-                        result++;}
-                }else{
-
-                    if(nums[j]-nums[i-1]==k){
-                   
-                      result++;}
-                }
+            }
+            if (map.containsKey(sum)) {
+                map.put(sum, map.get(sum) + 1); // Increment existing value
+            } else {
+                map.put(sum, 1); // Initialize new key with value 1
             }
         }
+       
         return result;
     }
 }
