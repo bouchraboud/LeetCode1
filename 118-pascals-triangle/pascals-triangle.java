8ea616a77c4a07a2962b69bfd5fc252a915bac16
bouchraboud <151152911+bouchraboud@users.noreply.github.com>
2025-03-01 15:55:39 +0000

    Time: 1 ms (84.08%) | Memory: 42 MB (58.54%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -11,25 +11,27 @@ class Solution {
         return result;
     }
 
-    private List<Integer> fun(int numsRow, List<List<Integer>> generate) {
+    public List<Integer> fun(int numsRow, List<List<Integer>> generate) {
         List<Integer> result = new ArrayList<>();
         if (numsRow == 1) {
             result.add(1);
             generate.add(result);
             return result;
+        } else {
+            result.add(1);
+            List<Integer> prevL = fun(numsRow - 1, generate);
+            result.add(1);
+            if(numsRow==2){ 
+             generate.add(result);
+             return result;}
+            for (int i = 1; i <=(numsRow-1)/2; i++) {
+                int val = prevL.get(i) + prevL.get(i - 1);
+                result.add(i,val);
+                if(result.size()<numsRow) result.add(i+1,val);
+            }
+            
+            generate.add(result);
+            return result;
         }
-        List<Integer> prevL = fun(numsRow - 1, generate);
-        result.add(1);
-        int half = (numsRow - 1) / 2;
-        for (int i = 1; i <= half; i++) {
-            int val = prevL.get(i - 1) + prevL.get(i);
-            result.add(val);
-        }
-        // Mirror the computed values for symmetry
-        for (int i = (numsRow % 2 == 0) ? half : half - 1; i >= 0; i--) {
-            result.add(result.get(i));
-        }
-        generate.add(result);
-        return result;
     }
 }
