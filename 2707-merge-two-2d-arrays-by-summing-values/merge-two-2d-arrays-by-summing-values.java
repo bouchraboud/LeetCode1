dbc2afd7ee83c7f73ea7eeded492c7d989027ae8
bouchraboud <151152911+bouchraboud@users.noreply.github.com>
2025-03-02 15:14:45 +0000

    Time: 5 ms (28.44%) | Memory: 45.3 MB (61.98%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -2,33 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        // Use TreeMap to automatically sort the keys
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        // Use HashMap for O(1) insertions and lookups
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Add all elements from nums1 to the map
-        for (int i = 0; i < nums1.length; i++) {
-            map.put(nums1[i][0], nums1[i][1]);
+        // Merge nums1 into the map
+        for (int[] num : nums1) {
+            map.put(num[0], num[1]);
         }
 
-        // Merge elements from nums2 into the map
-        for (int i = 0; i < nums2.length; i++) {
-            if (map.containsKey(nums2[i][0])) {
-                // If the key already exists, add the value from nums2 to the existing value
-                map.put(nums2[i][0], map.get(nums2[i][0]) + nums2[i][1]);
-            } else {
-                // If the key does not exist, add the key-value pair from nums2
-                map.put(nums2[i][0], nums2[i][1]);
-            }
+        // Merge nums2 into the map
+        for (int[] num : nums2) {
+            // If the key already exists, add the value from nums2 to the existing value
+            map.put(num[0], map.getOrDefault(num[0], 0) + num[1]);
         }
 
         // Convert the map to a 2D array
         int[][] result = new int[map.size()][2];
-        int i = 0;
+        int index = 0;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            result[i][0] = entry.getKey();
-            result[i][1] = entry.getValue();
-            i++;
+            result[index][0] = entry.getKey();
+            result[index][1] = entry.getValue();
+            index++;
         }
+
+        // Sort the result by the first element (ID)
+        Arrays.sort(result, (a, b) -> a[0] - b[0]);
 
         return result;
     }
