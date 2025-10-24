0ab3416a146dce90772b23b0ffaba15e6a6f1696
bouchraboud <boudiwanbouchra@gmail.com>
2025-10-24 15:06:33 +0100

    Time: 27 ms (5.44%), Space: 45.2 MB (6.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,48 +1,40 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> map=new HashMap<Character,Integer>();
-          
-          if(s.length()!=t.length()) return false;
-         
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
 
+        if (s.length() != t.length())
+            return false;
 
-          for(int i=0;i<s.length();i++){
-            
-           
-              if(map.containsKey(s.charAt(i))){
-               map.replace(s.charAt(i),map.get(s.charAt(i))+1);
-                   if(map.get(s.charAt(i))==0)  map.remove(s.charAt(i));
-                
-              }else{
-                map.put(s.charAt(i),1);
-              } 
-             
-          }
+        for (int i = 0; i < s.length(); i++) {
 
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+                if (map.get(s.charAt(i)) == 0)
+                    map.remove(s.charAt(i));
 
-          
-          for(int i=0;i<t.length();i++){
-              
-            
-              if(map.containsKey(t.charAt(i))){
-                    map.replace(t.charAt(i),map.get(t.charAt(i))-1);
-                    System.out.print(map.get(t.charAt(i)));
-                    if(map.get(t.charAt(i))==0)  map.remove(t.charAt(i));
-                
-                
-              }else{
-                map.put(s.charAt(i),-1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+
+            if (map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                if (map.get(t.charAt(i)) == 0)
+                    map.remove(t.charAt(i));
+
+            } else {
+                map.put(s.charAt(i), -1);
                 return false;
-                
-              }
-              
-              
-              
-          }
-          if (map.size()==0) return true;
-           return false;
 
-          
-     
+            }
+
+        }
+        if (map.size() == 0)
+            return true;
+        return false;
+
     }
 }
