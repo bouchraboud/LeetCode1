6e4ed2b6409fff1ff10c15dd704442d64ce6e78c
bouchraboud <boudiwanbouchra@gmail.com>
2025-11-06 21:27:32 +0100

    Time: 0 ms (100%), Space: 48.4 MB (5.8%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int target) {
+        if (left > right) return -1; 
+
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return binarySearch(nums, mid + 1, right, target);
+        } else {
+            return binarySearch(nums, left, mid - 1, target);
+        }
+    }
+}
