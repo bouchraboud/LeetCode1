416cca3ae5db07b72c85919d65c0d5c2ee70e7f3
bouchraboud <boudiwanbouchra@gmail.com>
2025-12-09 18:02:56 +0100

    Time: 794 ms (5.64%), Space: 47.3 MB (5.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,20 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
-        int[] frequency =new int[26];
-
-        for(int i=0;i<s1.length();i++){
-            frequency[s1.charAt(i)-'a']++;
-        }
-
-        String freqStr=Arrays.toString(frequency);
-
-        for(int i=0;i<s2.length();i++){
-            if(frequency[s2.charAt(i)-'a']!=0){
-                int[] temp =new int[26];
-                temp[s2.charAt(i)-'a']++;
-                int j=i+1;
-                while(j<s2.length()&&j-i<s1.length()){
-                    temp[s2.charAt(j)-'a']++;
-                    j++;
-                }
-                System.out.println(Arrays.toString(temp));
-                System.out.println(freqStr);
-                if(Arrays.toString(temp).equals(freqStr)) return true;
-            }
-        }
-
-        return false;
+        char[] s11 = s1.toCharArray();
+        Arrays.sort(s11);
+        String s1sorted=new String(s11);
             
-
+              for(int i=0;i<s2.length();i++){
+                      if(i+s1.length()-1<s2.length()&&s1.contains(String.valueOf(s2.charAt(i)))) {
+                            String curr=s2.substring(i,i+s1.length());
+                            char[] chars = curr.toCharArray();
+                            Arrays.sort(chars);
+                            String currSorted=new String(chars);
+                            if(currSorted.equals(s1sorted)) return true;       
+                      }
+            }
+            return false;
+        
           
     }
 }
