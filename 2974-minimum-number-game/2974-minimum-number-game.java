e7ac05d9b28a2401711a649349362db74fb4fdfa
bouchraboud <boudiwanbouchra@gmail.com>
2025-12-15 22:14:59 +0100

    Time: 5 ms (59.04%), Space: 47.1 MB (26.87%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i += 2) {
-            int temp = nums[i];
+            int tmp = nums[i];
             nums[i] = nums[i + 1];
-            nums[i + 1] = temp;
+            nums[i + 1] = tmp;
         }
 
         return nums;
