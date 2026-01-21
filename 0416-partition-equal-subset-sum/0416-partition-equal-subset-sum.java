89f6ea954c3aa87b179e881cfdb24dc9fa854b3d
bouchraboud <boudiwanbouchra@gmail.com>
2026-01-21 11:45:23 +0100

    Time: 91 ms (24.65%), Space: 75.6 MB (20.98%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n=nums.length;
-        int sum=0;
+        int n = nums.length;
+        int sum = 0;
+        for (int x : nums) sum += x;
 
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-        }
-        if(sum%2==1) return false;
-        int half=sum/2;
+        if (sum % 2 == 1) return false;
+        int half = sum / 2;
 
-
-        return helper(nums,0,n-1,half);
-        
+        Boolean[][] memo = new Boolean[n][half + 1];
+        return helper(nums, n - 1, 0, half, memo);
     }
 
-    private boolean helper(int[] nums,int sum ,int i,int half){
-        if(sum==half) return true;
-        if(sum>half||i<0) return false;
+    private boolean helper(int[] nums, int i, int sum, int half, Boolean[][] memo) {
+        if (sum == half) return true;
+        if (sum > half || i < 0) return false;
 
-        return helper(nums,sum+nums[i],i-1,half)||helper(nums,sum,i-1,half);
+        if (memo[i][sum] != null) return memo[i][sum];
 
+        boolean take = helper(nums, i - 1, sum + nums[i], half, memo);
+        boolean skip = helper(nums, i - 1, sum, half, memo);
+
+        return memo[i][sum] = take || skip;
     }
 }
